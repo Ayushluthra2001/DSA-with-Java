afc8029a65abdd2cdd15f56e3e3915bfb7a37c33
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-06-12 10:54:22 +0530

    Create Count Primes.java

---
+++ Count Primes.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        int count=0;
+        int arr[] = new int[n+1];
+        arr[0]=1;
+        arr[1]=1;
+        for(int i=2;i*i<n;i++){
+            if(arr[i]==0){
+                for(int j=i*i;j<n;j+=i){
+                    arr[j]=1;
+                }
+            }
+        }
+        for(int i=2;i<n;i++){
+            if(arr[i]==0) count++;
+        }
+        return count;
+    }
+}
